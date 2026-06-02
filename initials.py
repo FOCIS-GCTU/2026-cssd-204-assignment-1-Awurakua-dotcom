89ce751de2d474b67564c8d7ec9eb08bd7e77fb1
Awurakua-dotcom <2425402950@live.gctu.edu.gh>
2026-06-01 23:34:37 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,27 @@
+#File: JAE.py
+#Description: Print Out My Initials
+#Assignment Number: 1
+#
+#Name: Juliet Essel
+#Student ID: 2425402950
+#Email: 2425402950@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor, Juliet Essel, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
-
-
-
+def main () :
+    print()
+    print("...JAE")
+    print()
+    print("...JJJJJJJJJJJJ............AAAA............EEEEEEEEEEEE")
+    print("...JJJJJJJJJJJJ............AAAA............EEEEEEEEEEEE")
+    print(".........JJJJJJ...........AA..AA...........EEEE........")
+    print(".........JJJJJJ...........AA..AA...........EEEE........")
+    print(".........JJJJJJ..........AAAAAAAA..........EEEEEEEEEEEE")
+    print(".........JJJJJJ..........AAAAAAAA..........EEEE........")
+    print("...JJJJ..JJJJJJ.........AAA....AAA.........EEEE........")
+    print("...JJJJJJJJJJJJ...**....AAA....AAA....**...EEEEEEEEEEEE")
+    print(".....JJJJJJJJ.....**...AAAA....AAAA...**...EEEEEEEEEEEE")
+    print()
 main()
